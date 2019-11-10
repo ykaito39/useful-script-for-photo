1b7fba5c0675740645240fe8bb1bcf338a68af2c
ykaito39 <ykaito39@gmail.com>
2019-11-10 23:45:29 +0900

    delete memo on source code

--- 01_RAW画像仕分けくん/RAW画像仕分けくん_v0.1.py
+++ 01_RAW画像仕分けくん/RAW画像仕分けくん_v0.1.py
@@ -27,37 +27,6 @@ except os.error:
 
 file_list = sorted(file_list_org)
 
-"""
-#実験コード:ファイルを消すとリストの中はどうなるのか...?
-print(file_list)
-
-print(file_list[0] + "を削除します。")
-os.remove(os.path.abspath(file_list[0]))
-"""  
-print(file_list) #:List内には居座ったまま
-
-
-"""
-print(file_list[1][-3:]) #:"RAF"
-"""
-
-
-"""
-tmp = ""
-for file_name in file_list:
-    if file_name[-3:] == ".py":
-        continue
-    
-    if (file_name[:-4] == tmp) or ( file_name[:-4]+".RAF" in file_list ):
-        print(file_name[:-4] + "は2つあるよ!さくじょしないよ")
-    else:
-        print(file_name + "は一個しか無いから削除します(無慈悲)。")
-        #os.remove(os.path.abspath(file_list[i]))
-    tmp = file_name[:-4]
-    
-"""
-print(file_list[0][-3:])
-
 #とりあえず動くけど本当にクソコードですありがとうございました。
 i = 0
 while True:
@@ -81,22 +50,3 @@ while True:
         break
     
     i += 1
-    
-    
-"""
-for i in range(len(file_list)):
-    print(i) #:0~N
-    
-    if file_list[i][-3:] == ".py":
-        continue
-
-    if file_list[i][:-4] == file_list[i+1][:-4]:
-        print("削除非対象:" + file_list[i] + " " + file_list[i+1])
-        i += 2 #なぜ足し算されない??? = i in range()はループ回数目毎にrangeを呼んでiに代入しているのでは...
-    else: #RAFもJPEGも存在するやつは残しておく
-        print("ファイル名不一致:" + file_list[i] + "を削除します。")
-        #os.remove(os.path.abspath(file_list[i]))
-    """        
-#    except IndexError:
-#        print("pass")
-#        break
